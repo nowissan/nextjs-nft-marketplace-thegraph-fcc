ebfae170fe0e75c7d3dfe5099f7055ccaf6baf9f
Yoshitaka Shimokawa <simokawa@hikutas0001.local>
2023-04-20 23:20:57 +0900

    initial commit 2

--- pages/sell-nft.js
+++ pages/sell-nft.js
@@ -64,7 +64,7 @@ export default function Home() {
             onSuccess: () => handleListSuccess(),
             onError: (error) => {
                 console.log(error),
-            },
+            }
         })
     }
 
